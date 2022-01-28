4c22d49a9d7f6d94c450e25fd9fa829fdf31b486
Martin <mart256@gmail.com>
2022-01-28 23:59:49 +0100

    abstract methods cannot have body. var keyword.

--- src/sandbox/Playing.java
+++ src/sandbox/Playing.java
@@ -6,5 +6,16 @@ public class Playing {
         float aValue = 10.0f;
 
         System.out.println("And the value is: " + aValue);
+
+        var someValue = 25;   // var keyword
+    }
+}
+
+abstract class AnAbstractClass {
+
+    public abstract void swim();   // abstract method cannot have a body
+
+    public void pool() {
+        swim();
     }
 }
