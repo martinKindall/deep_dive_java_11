297abcc43d31fbf1c3a465ec5222a0566bc9e30d
Martin <mart256@gmail.com>
2022-01-28 23:13:45 +0100

    Initial commit

---
+++ src/sandbox/Playing.java
@@ -0,0 +1,10 @@
+package sandbox;
+
+public class Playing {
+
+    public static void main(String[] args) {
+        float aValue = 10.0f;
+
+        System.out.println("And the value is: " + aValue);
+    }
+}
